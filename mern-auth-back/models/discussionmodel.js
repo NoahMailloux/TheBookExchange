80f613c3541f3e20606f169ff2095c5a106c47a1
xirius02 <49013402+xirius02@users.noreply.github.com>
2021-01-31 12:12:24 -0500

    erick

--- mern-auth-back/models/discussionmodel.js
+++ mern-auth-back/models/discussionmodel.js
@@ -4,8 +4,8 @@ const mongoose = require("mongoose");
 
 const discussionSchema = new mongoose.Schema({
     
-    title: {type: String, required: true, minlength: 5},
-    creator: {type: String, required: true}
+    title: {type: String},
+    creator: {type: String}
 
 });
 
--- mern-auth-back/routes/testRoutes/ericktemproutes.js
+++ mern-auth-back/routes/testRoutes/ericktemproutes.js
@@ -3,12 +3,23 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const auth = require("../../middleware/auth");
 const User = require("../../models/userModel");
+const Discussion = require("../../models/discussionmodel")
 
 /*Ignore this */
+//Find a user
+router.get("/", auth, async (req, res) =>{
+  const user = await User.findById(req.user);
+  res.json({
+      displayName: user.displayName,
+      id: user._id,
+  });
+});
+//
 
 
 router.post("/creatediscussion", async (req, res) =>{
 //comments
+const discussion = await Discussion.findById(req.user);
 const {title, creator } = req.body;
 try{
   const token = req.header("x-auth-token");
@@ -20,11 +31,14 @@ try{
   const user = await User.findById(verified.id);
   if(!user) return res.json(false);
 
-  const newDiscussion = new User({
-    email,
-    password: passwordHash,
-    displayName
+  const newDiscussion = new Discussion({
+    title,
+    creator: user.displayName,
 });
+const creatediscussion = await newDiscussion.save();
+        res.json(creatediscussion);
+
+
   return res.json(true);
  }catch(err){ 
      res.status(500).json({error: err.message});
