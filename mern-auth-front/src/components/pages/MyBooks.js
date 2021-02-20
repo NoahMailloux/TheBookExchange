9d0a36eaca3eea4d9520887a7092e60c48fe311c
NoahMailloux <72894973+NoahMailloux@users.noreply.github.com>
2021-02-19 22:14:17 -0500

    add-nbm

--- mern-auth-front/src/components/pages/MyBooks.js
+++ mern-auth-front/src/components/pages/MyBooks.js
@@ -1,65 +1,61 @@
-import React, {useEffect, useContext}  from 'react';
-import {useHistory} from "react-router-dom";
+import React, { useEffect, useContext } from 'react';
+import { useHistory } from "react-router-dom";
 import UserContext from "../../context/UserContext";
 import AuthOptions from '../auth/AuthOptions';
 import LoggedInHeader from '../layout/LoggedInHeader';
 import "./myBooks.css";
+import Popup from 'reactjs-popup';
 
 
 
 export default function MyBooks() {
-    const {userData} = useContext(UserContext);
+    const { userData } = useContext(UserContext);
     const history = useHistory();
     const register = () => history.push("/register")
     const numbers = ['one', 'two', 'three', 'four'];
     const items = []
-    for(const number of numbers){
+    for (const number of numbers) {
         items.push(
-            
-            <>{<div className="myBook1"></div>}
-            {<button onClick={register} className="bookDetailsBtn">Details</button>}</>
-        
+
+            <>
+                {<div className="myBook1"></div>}
+                  <Popup trigger={<button> Trigger</button>} position="right center">
+                    <div>Popup content here !!</div> 
+                </Popup>
+            </>
+
         )
     }
- //   function detailsOnClick() {
-  //      popup.alert({
-  //          content: 'Hello!'
-  //      });
-   // }
-/*
-    const elements = [{bookImg:"myBook1",bookDetails:"Details"},
-        {bookImg:"myBook1",bookDetails:"Details2"},
-        {bookImg:"myBook1",bookDetails:"Details3"},];
-        {elements.map((value, index) => {
-            var result = value.keys(obj).map((key) => [index(key), obj[key]]);
-          })}
-
-          //below table
-        {elements.map((value, index) => {
-        return <tr key={index}>{value}
-        
-        {JSON.stringify(value.bookImg)}
-        {JSON.stringify(value.bookDetails)}
-
-          })} below tr
-*/
-    return(
-        <div>
-        <LoggedInHeader />
-        <h1>My Books</h1>
-        {items}
-
-        
-        <table >
-        <tr>
-        <td>
-
-        </td>
-        </tr>
-      
-
-        </table>
+    /*Adjust z index for popup */
+    /*
+        const elements = [{bookImg:"myBook1",bookDetails:"Details"},
+            {bookImg:"myBook1",bookDetails:"Details2"},
+            {bookImg:"myBook1",bookDetails:"Details3"},];
+            {elements.map((value, index) => {
+                var result = value.keys(obj).map((key) => [index(key), obj[key]]);
+              })}
     
+              //below table
+            {elements.map((value, index) => {
+            return <tr key={index}>{value}
+            
+            {JSON.stringify(value.bookImg)}
+            {JSON.stringify(value.bookDetails)}
+    
+              })} below tr
+    */
+    return (
+        <div>
+            <LoggedInHeader />
+            <h1>My Books</h1>
+            <table>
+                <tr>
+                    <td>
+                        {items}
+                    </td>
+                </tr>
+            </table>
+
         </div>
     );
 
