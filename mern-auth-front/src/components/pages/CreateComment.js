7f7b52d38ab5f7288ff788be831ffa722fb59de9
xirius02 <49013402+xirius02@users.noreply.github.com>
2021-02-22 14:12:01 -0500

    update

---
+++ mern-auth-front/src/components/pages/CreateComment.js
@@ -0,0 +1,127 @@
+import React, {useEffect, useContext, useState, useCallback}  from 'react';
+import {useHistory} from "react-router-dom";
+import UserContext from "../../context/UserContext";
+import AuthOptions from '../auth/AuthOptions';
+import "./CreateComment.css"
+import LoggedInHeader from '../layout/LoggedInHeader';
+import axios from 'axios';
+
+export default function CreateComment() {
+
+    
+    const [discussions, setDiscussions] = useState([])
+    const {userData} = useContext(UserContext);
+    const history = useHistory();
+
+    /*  DO NOT USE
+    const authAxios = axios.create({
+        baseURL: "http://localhost:5001/discussion/listdiscussions",
+        headers: {
+            Authorization: userData.token
+        }
+    })*/
+
+/* TABLE GENERATOR TEST
+    const tableGenerator = () => {
+        return (
+          <table>
+            <tr>
+            </tr>
+          </table>
+        );
+      }
+*/
+    const fetchDiscussions = useCallback(async() => {
+        
+        try{
+
+            const loaddiscussions = await axios.post("http://localhost:5001/discussion/listdiscussions", 
+            {
+                headers: {
+                    "x-auth-token": userData.token,
+                    "Content-Type": "text/json"
+                }
+            })
+            this.state(loaddiscussions)
+        }catch(err){
+            console.log(err.message)
+        }
+    })
+
+
+    /*
+    useEffect(() =>  {
+
+        async function getdiscussions(){
+            
+
+        const loaddiscussions = await axios.post("http://localhost:5001/discussion/listdiscussions", 
+        {
+            headers: {
+                "x-auth-token": userData.token,
+                "Content-Type": "text/json"
+            }
+        })
+        setState(loaddiscussions)
+        }
+
+        getdiscussions()
+        //console.log(loaddiscussions)
+    }, [more])
+    */
+        //e.preventDefault();
+
+    return(
+      
+    <body id="bodycolor">
+
+    <LoggedInHeader />
+    
+
+<div class="container">
+<form action="google.com">
+<h3 for="fname">Title</h3>
+    <input type="text" id="title" name="title" placeholder="Title"></input>
+
+    <h3 for="lname">Book</h3>
+    <input type="text" id="Book" name="Book" placeholder="Book"></input>
+
+    <h3 for="lname">Genre</h3>
+    <input type="text" id="Genre" name="Genre" placeholder="Genre"></input>
+
+    <h3 for="lname">Comment</h3>
+    <br></br>
+    <textarea id="Genre" name="Comment" placeholder="Insert Comment" rows="6" cols="50"></textarea>
+        <br></br>
+    <button id="create" class="sub" name="create">Create</button>
+</form>
+</div>
+    </body>
+
+    );
+}
+
+    {/*
+        <form className="discussionsform" >
+        <table>
+            <thead>
+
+            <tr>
+                <th>Title</th>
+                <th>Creator</th>
+                <th>Book</th>
+                <th>Genre</th>
+            </tr>
+            </thead>
+            <tbody>
+                {}
+            <tr>
+                <td>Title</td>
+                <td>Creator</td>
+                <td>Book</td>
+                <td>Genre</td>
+            </tr>
+            </tbody>
+        </table>
+        </form>
+    */}
---
+++ mern-auth-front/src/components/pages/ViewDiscussion.js
@@ -0,0 +1,143 @@
+import React, {useEffect, useContext, useState, useCallback}  from 'react';
+import {useHistory} from "react-router-dom";
+import UserContext from "../../context/UserContext";
+import AuthOptions from '../auth/AuthOptions';
+import "./ViewDiscussion.css";
+import LoggedInHeader from '../layout/LoggedInHeader';
+import axios from 'axios';
+
+export default function Discussions() {
+
+    
+    const [discussions, setDiscussions] = useState([])
+    const {userData} = useContext(UserContext);
+    const history = useHistory();
+
+    /*  DO NOT USE
+    const authAxios = axios.create({
+        baseURL: "http://localhost:5001/discussion/listdiscussions",
+        headers: {
+            Authorization: userData.token
+        }
+    })*/
+
+/* TABLE GENERATOR TEST
+    const tableGenerator = () => {
+        return (
+          <table>
+            <tr>
+            </tr>
+          </table>
+        );
+      }
+*/
+    const fetchDiscussions = useCallback(async() => {
+        
+        try{
+
+            const loaddiscussions = await axios.post("http://localhost:5001/discussion/listdiscussions", 
+            {
+                headers: {
+                    "x-auth-token": userData.token,
+                    "Content-Type": "text/json"
+                }
+            })
+            this.state(loaddiscussions)
+        }catch(err){
+            console.log(err.message)
+        }
+    })
+
+
+    /*
+    useEffect(() =>  {
+
+        async function getdiscussions(){
+            
+
+        const loaddiscussions = await axios.post("http://localhost:5001/discussion/listdiscussions", 
+        {
+            headers: {
+                "x-auth-token": userData.token,
+                "Content-Type": "text/json"
+            }
+        })
+        setState(loaddiscussions)
+        }
+
+        getdiscussions()
+        //console.log(loaddiscussions)
+    }, [more])
+    */
+        //e.preventDefault();
+
+        const elements = ['one', 'two', 'three', 'four'];
+        const elements2 = ['5', '6', '7', '8'];
+    return(
+      <body id="bodycolor">
+    <div>
+    <LoggedInHeader />
+      
+    <table id="mainline" class="mainline line">
+        <tr>
+                <th>Title</th>
+                <th>Creator</th>
+                <th>Book</th>
+                <th>Genre</th>
+            </tr>
+      </table>
+      <table id="secondaryline" class="secondaryline">
+    <td>
+      {elements.map((value, index) => {
+        return <tr key={index}>{elements[0]}</tr>
+      })}
+    </td>
+    <td>
+      {elements.map((value, index) => {
+        return <tr key={index}>{elements[0]}</tr>
+      })}
+    </td>
+    <td>
+      {elements.map((value, index) => {
+        return <tr key={index}>{elements[0]}</tr>
+      })}
+    </td>
+    <td>
+      {elements.map((value, index) => {
+        return <tr key={index}>{elements[0]}</tr>
+      })}
+    </td>
+    </table>
+
+    </div>
+    <div>
+      <img width="335" height="500" src="/TheBookExchange/mern-auth-front/src/images/91AVsmqBaML.jpg" class="thumbnail" id="thumbnail" ></img>
+    </div>
+    </body>
+    );
+}
+
+    {/*
+        <form className="discussionsform" >
+        <table>
+            <thead>
+
+            <tr>
+                <th>Title</th>
+                <th>Creator</th>
+                <th>Book</th>
+                <th>Genre</th>
+            </tr>
+            </thead>
+            <tbody>
+                {}
+            <tr>
+                <td>Title</td>
+                <td>Creator</td>
+                <td>Book</td>
+                <td>Genre</td>
+            </tr>
+            </tbody>
+        </table>
+        </form>
+    */}
